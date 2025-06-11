c837d5d9b639caeba37dbbbb15e2c391c251a3a4
dev-ankit24 <ankitkumar839282@gmail.com>
2025-06-11 18:56:46 +0530

    footer color change

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -6,7 +6,7 @@ import { MdOutlineMailOutline } from 'react-icons/md'
 export default function Footer() {
   return (
     <>
-<footer>
+<footer  >
   {/* <!-- Top Section with Quote and Email Signup --> */}
   {/* <div className="bg-light py-5 text-center text-md-start">
     <div className="container">
@@ -26,12 +26,12 @@ export default function Footer() {
   </div> */}
 
   {/* <!-- Bottom Section with Green Background --> */}
-  <div className=" text-white mt-5 py-5" style={{background:"#b5b7bb"}}>
+  <div className=" text-white mt-5 py-5" style={{background:"#004B49"}}>
     <div className="container">
       <div className="row">
         {/* <!-- Logo & Certification --> */}
         <div className="col-md-4 text-center text-md-start mb-4 mb-md-0">
-            <h3 className='text-dark'>SilverSrooms</h3>
+            <h3 style={{color:"#e2c3a5"}}>SilverSrooms</h3>
           {/* <img src="./assets/img/" alt="Mother Earth Logo" style={{maxWidth: "180px"}} className="mb-3" /> */}
           {/* <div className="d-flex gap-2 justify-content-center justify-content-md-start">
             <img src="usda.png" alt="USDA" width="40" />
@@ -41,16 +41,16 @@ export default function Footer() {
 
         {/* <!-- Contact Info --> */}
         <div className="col-md-4 mb-4 mb-md-0 text-center text-md-start">
-          <h6 className="fw-bold t  text-dark" >CONTACT</h6>
+          <h6 className="fw-bold " style={{color:"#e2c3a5"}} >CONTACT</h6>
           <p className="mb-1" > <FaPhoneAlt className='me-2' />+91 9953992143</p>
           <p><MdOutlineMailOutline className='me-2' />info@perfectconsultancy.co</p>
-          <h6 className="fw-bold">HOURS</h6>
+          <h6 className="fw-bold" style={{color:"#e2c3a5"}}>HOURS</h6>
           <p>MON–FRI 8AM–5PM</p>
         </div>
 
         {/* <!-- Address & Social Icons --> */}
         <div className="col-md-4 text-center text-md-start">
-          <h6 className="fw-bold text-dark">ADDRESS</h6>
+          <h6 className="fw-bold " style={{color:"#e2c3a5"}}>ADDRESS</h6>
           <p><IoLocation className='me-2'/>A-1, First Floor Pehlad Market, <br/>Karol Bagh</p>
           <div className="mt-3">
             <a href="#" className="text-white me-3"><i className="fab fa-facebook fa-lg"></i></a>
